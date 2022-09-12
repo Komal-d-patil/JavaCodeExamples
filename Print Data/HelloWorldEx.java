a258502def1b43ceb5113688f8950dcba0a2d462
Komal-d-patil <113327674+Komal-d-patil@users.noreply.github.com>
2022-09-12 08:20:15 +0300

    Add files via upload

---
+++ Print Data/HelloWorldEx.java
@@ -0,0 +1,10 @@
+package printdata;
+
+public class HelloWorldEx {
+
+	public static void main(String args[])
+	{
+		System.out.println("Hi"+"\n"+"My first Example");
+		System.out.println("Hello World!!!");
+	}
+}
---
+++ Print Data/PrintDataEx.java
@@ -0,0 +1,20 @@
+package printdata;
+
+public class PrintDataEx {
+
+	public static void main(String args[])
+	{
+		int num = 7;
+		float b = 5.9f;
+		char letter = 'K';
+		boolean bool = true;
+		String mystr = "Komal";
+		double d=5.5;
+		System.out.println(num);
+		System.out.println(b);
+		System.out.println(letter);
+		System.out.println(bool);
+		System.out.println(mystr);
+		System.out.println(d);
+	}
+}
